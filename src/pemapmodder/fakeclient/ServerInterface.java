ed8762227b985fcb3be6831365f513f0e798f58d
PEMapModder <pemapmodder1970@gmail.com>
2014-05-18 22:45:18 +0800

    Small things

--- src/pemapmodder/fakeclient/ServerInterface.java
+++ src/pemapmodder/fakeclient/ServerInterface.java
@@ -4,14 +4,19 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
+import pemapmodder.fakeclient.android.ConnectServerService;
 import pemapmodder.fakeclient.packet.OpenConnectionRequestPacket;
+import pemapmodder.fakeclient.packet.Utils;
+import android.content.Context;
 
 public class ServerInterface extends Thread{
 	public final InetSocketAddress address;
 	public final DatagramSocket socket;
+	public final Context ctx;
 	protected long time;
-	public ServerInterface(InetSocketAddress address) throws SocketException{
+	public ServerInterface(InetSocketAddress address, Context ctx) throws SocketException{
 		this.address = address;
+		this.ctx = ctx;
 		socket = new DatagramSocket(address);
 	}
 	public void run(){
@@ -22,6 +27,11 @@ public class ServerInterface extends Thread{
 				socket.send(new OpenConnectionRequestPacket(System.nanoTime() - time).toUDP());
 				DatagramPacket pack = new DatagramPacket(new byte[128], 128);
 				socket.receive(pack);
+				byte[] data = pack.getData();
+				if(data[0] == Utils.I.UNCONNECTED_PING_OPEN_CONNECTIONS){
+					received = true;
+					break;
+				}
 			}catch(Exception e){
 				err(e);
 			}
@@ -29,8 +39,9 @@ public class ServerInterface extends Thread{
 		if(!received){
 			err(new Exception("No reponse from server for 5 seconds"));
 		}
+		
 	}
 	protected void err(Throwable t){
-		// TODO
+		((ConnectServerService)ctx).err(t);
 	}
 }
--- src/pemapmodder/fakeclient/android/ConnectServerService.java
+++ src/pemapmodder/fakeclient/android/ConnectServerService.java
@@ -1,16 +1,28 @@
 package pemapmodder.fakeclient.android;
 
+import pemapmodder.fakeclient.ServerInterface;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
 
 public class ConnectServerService extends Service{
+	public ServerInterface client;
 	public ConnectServerService(Bundle args){
-		
+		try{
+			client = new ServerInterface(null, this);
+		}catch(Exception e){
+			err(e);
+		}
 	}
-	@Override
-	public IBinder onBind(Intent intent){
+	@Override public int onStartCommand(Intent intent, int flags, int flagID){
+		client.start();
+		return START_NOT_STICKY;
+	}
+	@Override public IBinder onBind(Intent intent){
 		return null;
+	}
+	public void err(Throwable e){
+		// TODO
 	}
 }
