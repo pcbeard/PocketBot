d77087d4e369b594c03e049421430d30a9604230
PEMapModder <pemapmodder1970@gmail.com>
2014-09-13 16:14:50 +0800

    App: first commit

---
+++ src/com/github/legendofmcpe/pocketbot/android/Main.java
@@ -0,0 +1,27 @@
+package com.github.legendofmcpe.pocketbot.android;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main extends ActionBarActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		int id = item.getItemId();
+		if(id == R.id.action_settings){
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+    }
+}
